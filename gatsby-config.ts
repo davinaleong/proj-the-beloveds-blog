8dcf9aa84919c693517c0dd78d0eba233bc69c50
Davina Leong <davina.leong.sy@gmail.com>
2022-04-21 06:31:46 +0800

    Imported images

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -16,7 +16,20 @@ const config: GatsbyConfig = {
         "path": "./src/pages/"
       },
       __key: "pages"
-    }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        display: "standalone",
+        icon: "src/images/icon.png",
+        crossOrigin: `use-credentials`,
+      },
+    },
   ]
 };
 
