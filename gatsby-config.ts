05cef6f00dbc9154bfd0cd8c45bbc3cc3a140c86
Personal Profile <davina.leong.sy@gmail.com>
2022-04-20 18:54:40 +0800

    Installed sass

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,14 +5,19 @@ const config: GatsbyConfig = {
     title: `The Beloved's Blog`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sass",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
+    }
+  ]
 };
 
 export default config;
