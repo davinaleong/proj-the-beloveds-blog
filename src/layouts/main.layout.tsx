2cc402046c3d89ca05dddca801cd765f148d010e
Davina Leong <davina.leong.sy@gmail.com>
2022-04-22 00:38:56 +0800

    Added meta content

--- src/layouts/main.layout.tsx
+++ src/layouts/main.layout.tsx
@@ -1,4 +1,8 @@
 import * as React from "react"
+import { Helmet } from "react-helmet"
+
+// config
+import config from "../data/config"
 
 // components
 import HeaderComponent from "../components/header.component"
@@ -25,11 +29,32 @@ class MainLayout extends React.Component {
     }
 
     render = () => {
-        const { children, bgColor } = this.props
+        const { children, meta, bgColor } = this.props
         const { expandMenu } = this.state
+
+        const localMeta = {
+            author: config.meta.author,
+            siteUrl: config.meta.siteUrl,
+            title: config.meta.default.title,
+            keywords: config.meta.default.keywords,
+            description: config.meta.default.description
+        };
+      
+        if (meta && meta !== undefined) {
+            localMeta.title = meta.title;
+            localMeta.keywords = meta.keywords;
+            localMeta.description = meta.description;
+        }
 
         return (
             <div className={ bgColor }>
+                <Helmet>
+                    <meta name="description" content={localMeta.description} />
+                    <meta name="keywords" content={localMeta.keywords} />
+                    <meta name="author" content={localMeta.author} />
+                    <title>{localMeta.title}</title>
+                    <link rel="canonical" href={localMeta.siteUrl} />
+                </Helmet>
                 <HeaderComponent expandMenu={expandMenu} />
 
                 { children }
