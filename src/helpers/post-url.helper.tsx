163f025cef34ef1b63b3a43a8afc2adf57e19b68
Personal Profile <davina.leong.sy@gmail.com>
2022-04-22 16:13:55 +0800

    Added archive url helper

--- src/helpers/post-url.helper.tsx
+++ src/helpers/post-url.helper.tsx
@@ -1,15 +1,5 @@
-import * as React from "react"
-
-const PostUrlHelper = (slug: String, offset: Number = 0, page: Number = 0) => {
-    let uri = `?slug=${slug}`
-    if (offset) {
-        uri += `&offset=${offset}`
-    }
-    if (page) {
-        uri += `&page=${page}`
-    }
-
-    return `post${uri}`
+const PostUrlHelper = (slug: String) => {
+    return `post?slug=${slug}`
 }
 
 export default PostUrlHelper
